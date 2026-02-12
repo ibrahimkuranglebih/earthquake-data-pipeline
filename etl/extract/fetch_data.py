56d2da418ad46f2a286668318bf6f2dd63dcc22c
ibrahimkuranglebih <mumtazsamadikun765@gmail.com>
2026-02-12 11:47:14 +0700

    some adding of etl process

--- etl/extract/fetch_data.py
+++ etl/extract/fetch_data.py
@@ -4,6 +4,13 @@ import requests
 
 BASE_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query"
 
+params = {
+    "format": "geojson",
+    "starttime": "2026-01-01",
+    "endtime": "2026-02-01",
+    "minmagnitude": 4
+}
+
 response = requests.get(BASE_URL, params=params)
 data = response.json()
 print(data)
